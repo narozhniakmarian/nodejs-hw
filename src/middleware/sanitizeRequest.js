b0ddc22d9672459c84283cdcb131372fbdea9a23
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-11-02 13:56:44 +0100

    /sanitizeRequest

--- src/middleware/sanitizeRequest.js
+++ src/middleware/sanitizeRequest.js
@@ -7,8 +7,23 @@ export const sanitizeRequest = (req, res, next) => {
     return next();
   }
 
-  req.body = sanitize(req.body);
-  req.query = sanitize(req.query);
-  req.params = sanitize(req.params);
+  if (req.body && typeof req.body === 'object') {
+    for (const key in req.body) {
+      req.body[key] = sanitize(req.body[key]);
+    }
+  }
+
+  if (req.query && typeof req.query === 'object') {
+    for (const key in req.query) {
+      req.query[key] = sanitize(req.query[key]);
+    }
+  }
+
+  if (req.params && typeof req.params === 'object') {
+    for (const key in req.params) {
+      req.params[key] = sanitize(req.params[key]);
+    }
+  }
+
   next();
 };
