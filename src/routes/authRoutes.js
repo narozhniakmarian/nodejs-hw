1889d295ef760d0dd9ef37b7a552d1eb6a8c8fa2
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-11-10 15:56:04 +0100

    //

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@
 
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, logoutUser, refreshSession, registerUser, requestResetEmail, resetPassword } from '../controllers/authController.js';
+import { loginUser, logoutUser, refreshUserSession, registerUser, requestResetEmail, resetPassword } from '../controllers/authController.js';
 import { loginUserSchema, registerUserSchema, requestResetEmailSchema, resetPasswordSchema } from '../validations/authValidation.js';
 
 const router = Router();
@@ -12,7 +12,7 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 
 router.post('/auth/logout', logoutUser);
-router.post('/auth/refresh', refreshSession);
+router.post('/auth/refresh', refreshUserSession);
 
 router.post(
   '/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
