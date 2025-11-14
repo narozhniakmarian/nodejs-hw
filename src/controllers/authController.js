67fe8a11ec6bdc33d022512df78d05320b2a0469
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-11-14 22:32:46 +0100

    /13

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -107,7 +107,7 @@ export const requestResetEmail = async (req, res, next) => {
   const user = await User.findOne({ email });
   if (!user) {
     return res.status(200).json({
-      message: 'If this email exists, a reset link has been sent',
+      message: 'Password reset email sent successfully',
     });
   }
 
